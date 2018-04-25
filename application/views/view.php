db65004f46cf305813c9aa30598526b1ccbe0264
andikasantos <you@example.com>
2018-04-25 14:21:18 +0700

    Add DB

---
+++ application/views/view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	Username <br>
+	<input type="text" name="username"><br>
+	Password <br>
+	<input type="password" name="password"><br>
+	Level
+	<select>
+		<option value="admin">admin</option>
+		<option value="user">User</option>
+	</select>
+	<input type="submit" name="submit" value="Simpan">
+</form>
+</body>
+</html>
